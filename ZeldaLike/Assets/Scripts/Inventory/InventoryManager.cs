64dd84573ba7fd2cf73e15150135adfe30e17b6e
lsw5955 <lsw5955@qq.com>
2019-10-25 00:12:59 +0800

    finish episode 70 Inventory - Displaying Items
    https://www.youtube.com/watch?v=IQwvxZQt9Aw&list=PL4vbr3u7UKWp0iM1WIfRjCDTI03u43Zfu&index=73

---
+++ ZeldaLike/Assets/Scripts/Inventory/InventoryManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryManager : MonoBehaviour
+{
+    [Header("Inventory Information")]
+    public PlayerInventory playerInventory;
+    [SerializeField]
+    private GameObject blankInventorySlot;
+    [SerializeField]
+    private GameObject inventoryPanel;
+    [SerializeField]
+    private Text descriptionText;
+    [SerializeField]
+    private GameObject useButton;
+    
+    void MakeInventorySlots()
+    {
+        if(playerInventory)
+        {
+            for(int i = 0; i< playerInventory.myInventory.Count;i++)
+            {
+                GameObject temp = Instantiate(blankInventorySlot, inventoryPanel.transform.position, Quaternion.identity);
+                temp.transform.SetParent(inventoryPanel.transform);
+
+                InventorySlot newSlot = temp.GetComponent<InventorySlot>();
+                if(newSlot)
+                {
+                    Debug.Log("我可是根红苗正!");
+                    newSlot.Setup(playerInventory.myInventory[i], this);
+                }
+            }
+        }
+    }
+
+    public void SetTextAndButton(string description, bool buttonActive)
+    {
+        descriptionText.text = description;
+        useButton.SetActive(buttonActive);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MakeInventorySlots();
+        SetTextAndButton("", false);  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ ZeldaLike/Assets/Scripts/Inventory/PlayerInventory.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Iventory", menuName = "Inventory/玩家背包(bag")]
+public class PlayerInventory : ScriptableObject
+{
+    public List<InventoryItem> myInventory = new List<InventoryItem>();
+}
